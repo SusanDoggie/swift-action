4e6cab0efafc0d0a2ceb16c84104b498a530def9
Susan Cheng <susan.doggie@gmail.com>
2020-04-08 16:47:36 +0800

    clean

--- dist/macos/build.sh
+++ dist/macos/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 
-SDK=${SDK:-macosx}
+if [ "${USE_XCOEBUILD}" = true ] ; then
 
-XCODEBUILD_CONFIG: "-configuration Release -sdk ${SDK}"
-SCHEMES=$(xcodebuild -list | grep --after-context=-1 '^\s*Schemes:' | tail -n +2 | xargs)
+  SDK=${SDK:-macosx}
 
-for SCHEME in ${SCHEMES}; do
-  echo "Building scheme ${SCHEME}"
-  xcodebuild ${XCODEBUILD_CONFIG} -scheme ${SCHEME} | xcpretty -f `xcpretty-travis-formatter`
-done
+  XCODEBUILD_CONFIG: "-configuration Release -sdk ${SDK}"
+  SCHEMES=$(xcodebuild -list | grep --after-context=-1 '^\s*Schemes:' | tail -n +2 | xargs)
+
+  for SCHEME in ${SCHEMES}; do
+    echo "Building scheme ${SCHEME}"
+    xcodebuild ${XCODEBUILD_CONFIG} -scheme ${SCHEME} | xcpretty -f `xcpretty-travis-formatter`
+  done
+
+else
+  swift build -c release --enable-test-discovery
+fi
--- dist/macos/test.sh
+++ dist/macos/test.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e
 
-SDK=${SDK:-macosx}
-DESTINATION=${DESTINATION:-'platform=macOS'}
-ENABLE_CODECOV=${DESTINATION:-false}
+if [ "${USE_XCOEBUILD}" = true ] ; then
 
-XCODEBUILD_CONFIG: "-configuration Release -sdk ${SDK}"
-SCHEMES=$(xcodebuild -list | grep --after-context=-1 '^\s*Schemes:' | tail -n +2 | xargs)
+  SDK=${SDK:-macosx}
+  DESTINATION=${DESTINATION:-'platform=macOS'}
+  ENABLE_CODECOV=${DESTINATION:-false}
 
-for SCHEME in ${SCHEMES}; do
-  echo "Testing scheme ${SCHEME}"
-  xcodebuild ${XCODEBUILD_CONFIG} -scheme ${SCHEME} build-for-testing -skipUnavailableActions | xcpretty -f `xcpretty-travis-formatter`
-  xcodebuild ${XCODEBUILD_CONFIG} -scheme ${SCHEME} -destination "${DESTINATION}" test-without-building -skipUnavailableActions -enableCodeCoverage ${ENABLE_CODECOV} | xcpretty -f `xcpretty-travis-formatter`
-done
+  XCODEBUILD_CONFIG: "-configuration Release -sdk ${SDK}"
+  SCHEMES=$(xcodebuild -list | grep --after-context=-1 '^\s*Schemes:' | tail -n +2 | xargs)
+
+  for SCHEME in ${SCHEMES}; do
+    echo "Testing scheme ${SCHEME}"
+    xcodebuild ${XCODEBUILD_CONFIG} -scheme ${SCHEME} build-for-testing -skipUnavailableActions | xcpretty -f `xcpretty-travis-formatter`
+    xcodebuild ${XCODEBUILD_CONFIG} -scheme ${SCHEME} -destination "${DESTINATION}" test-without-building -skipUnavailableActions -enableCodeCoverage ${ENABLE_CODECOV} | xcpretty -f `xcpretty-travis-formatter`
+  done
+
+else
+  swift test -c release --enable-test-discovery
+fi
