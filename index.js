d4e97965d927ac9868e29f1658493bff2056ff4e
Susan Cheng <susan.doggie@gmail.com>
2020-04-08 17:17:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const exec = require("@actions/exec");
 
 const action = core.getInput("action");
 
-exec.exec(`dist/${action}.sh`).catch(err => { core.setFailed(err.message); });
+exec.exec(`/github/workflow/SusanDoggie/swift-action/dist/${action}.sh`).catch(err => { core.setFailed(err.message); });
