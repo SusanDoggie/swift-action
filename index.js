a628774ff8bd46f70248f2bd65ec54652902e207
Susan Cheng <susan.doggie@gmail.com>
2020-04-08 18:27:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ const exec = require("@actions/exec");
 
 const action = core.getInput("action");
 
+const use_xcodebuild = core.getInput("use_xcodebuild");
+
+if (use_xcodebuild) {
+  
+  const sdk = core.getInput("sdk");
+  const destination = core.getInput("destination");
+  const enable_codecov = core.getInput("enable_codecov");
+  
+  core.exportVariable('SDK', sdk);
+  core.exportVariable('DESTINATION', destination);
+  core.exportVariable('ENABLE_CODECOV', enable_codecov);
+  
+}
+
 exec.exec(`${__dirname}/dist/${action}.sh`).catch(err => { core.setFailed(err.message); });
