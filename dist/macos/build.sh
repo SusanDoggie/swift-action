dd15ae65e7d21fc369e738ec2dbaddb684a2fc3f
Susan Cheng <susan.doggie@gmail.com>
2020-04-08 18:58:11 +0800

    Update build.sh

--- dist/macos/build.sh
+++ dist/macos/build.sh
@@ -4,10 +4,12 @@ set -e
 if [ "${USE_XCOEBUILD}" = true ] ; then
 
   SDK=${SDK:-macosx}
-
   XCODEBUILD_CONFIG="-configuration Release -sdk ${SDK}"
+  
+  echo "SDK: ${SDK}"
+  
   SCHEMES=$(xcodebuild -list | grep --after-context=-1 '^\s*Schemes:' | tail -n +2 | xargs)
-
+  
   for SCHEME in ${SCHEMES}; do
     echo "Building scheme ${SCHEME}"
     xcodebuild ${XCODEBUILD_CONFIG} -scheme ${SCHEME}
