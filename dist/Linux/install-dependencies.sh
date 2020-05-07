496f2362defecf4387c549296ef0ce027243200e
Susan Cheng <susan.doggie@gmail.com>
2020-05-07 16:11:32 +0800

    clean

--- dist/Linux/install-dependencies.sh
+++ dist/Linux/install-dependencies.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . /etc/os-release
 echo ">> NAME: ${NAME}"
 
-bash ${SCRIPT_DIR}/${NAME}/install-dependencies.sh
+bash '${SCRIPT_DIR}/${NAME}/install-dependencies.sh'
