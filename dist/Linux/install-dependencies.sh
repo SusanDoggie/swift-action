dbf40443110d6479643607abf74e6c459530fc43
Susan Cheng <susan.doggie@gmail.com>
2020-05-08 16:16:18 +0800

    clean

--- dist/Linux/install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-. /etc/os-release
-echo ">> NAME: ${NAME}"
-
-bash "${SCRIPT_DIR}/${NAME}/install-dependencies.sh"
--- dist/build.sh
+++ dist/build.sh
@@ -6,6 +6,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo ">> uname: $(uname)"
 SYSTEM="$(uname)"
 
-bash "${SCRIPT_DIR}/${SYSTEM}/install-dependencies.sh"
-
 bash "${SCRIPT_DIR}/${SYSTEM}/build.sh"
