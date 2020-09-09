8bd860f0008434000681e6c7a6807cfc0a9d6c75
Susan Cheng <susan.doggie@gmail.com>
2020-09-09 16:41:15 +0800

    Update build.sh

--- dist/Darwin/build.sh
+++ dist/Darwin/build.sh
@@ -6,16 +6,17 @@ CONFIGURATION=${CONFIGURATION:-release}
 if [ "${USE_XCODEBUILD}" = true ] ; then
 
   SDK=${SDK:-macosx}
-  XCODEBUILD_CONFIG="-configuration ${CONFIGURATION} -sdk ${SDK}"
+  DESTINATION=${DESTINATION:-'platform=macOS'}
   
   echo "CONFIGURATION: ${CONFIGURATION}"
   echo "SDK: ${SDK}"
+  echo "DESTINATION: ${DESTINATION}"
   
   SCHEMES=$(xcodebuild -list | grep --after-context=-1 '^\s*Schemes:' | tail -n +2 | xargs)
   
   for SCHEME in ${SCHEMES}; do
     echo "Building scheme ${SCHEME}"
-    xcodebuild ${XCODEBUILD_CONFIG} -scheme ${SCHEME}
+    xcodebuild -scheme ${SCHEME} -configuration ${CONFIGURATION} -sdk ${SDK} -destination "${DESTINATION}"
   done
 
 else
